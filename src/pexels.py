593ee875db20b96541522a18f858845133058c7b
Alexxx98 <aleksander.pres98@gmail.com>
2025-05-19 18:47:52 +0200

    simple photo and video search

---
+++ src/pexels.py
@@ -0,0 +1,46 @@
+"""
+Looking for photos and videos, that fits the generated text.
+Using pexels api.
+
+"""
+
+
+from dotenv import load_dotenv
+from typing import List
+
+import os
+import requests
+
+
+# Load environmental variables from the .env file
+load_dotenv()
+
+
+class Pexels:
+    standard_endpoint = "https://api.pexels.com/v1/"
+    video_endpoint = "https://api.pexels.com/videos/"
+    api_key = os.getenv("PEXELS_API_KEY")
+
+
+    def search_for_photo(self, query: str, curated: bool = False) -> List[str]:
+        """ Search the regular pexels photo library or the curated section """
+
+        section: str
+        section = "curated " if curated else "search"
+
+        response = requests.get(self.standard_endpoint + f"{section}?query={query}", headers={"Authorization": self.api_key})
+        if response.ok:
+            photos_data: dict = response.json()['photos']
+            photo_urls: List[str] = [photo["url"] for photo in photos_data]
+
+            return photo_urls
+        else:
+            return f"Error code: {response.status_code}"
+
+
+    def search_for_video(self) -> List[str]:
+        ...
+
+
+    def search_for_popular_video(self):
+        ...
