4bbae32ca41be5c522805b1d9973a462db591d0a
Alexxx98 <aleksander.pres98@gmail.com>
2025-05-22 21:32:20 +0200

    generate speech from text

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from src import gpt, tts, pexels
+"""
+Usage: python3 main.py "<prompt>"
+
+"""
+
+import sys
 
 
 def main() -> None:
     """ Function where the code is executed """
+
+    if len(sys.argv) < 2:
+        print('Error bad usage')
+        print('Proper usage: python3 main.py "<prompt>"\n')
+        return
+    
+    # Import modules
+    from src import gpt, tts, pexels
+
+    # Input as a string
+    prompt = sys.argv[1]
+
+    # Generate text from the prompt
+    text: str = gpt.generate_text(prompt)
+
+    # generate speech with subtitles from the generated text
+    # with the method's default voice
+    tts.generate_speech(text)
 
 
 # Execute the app
