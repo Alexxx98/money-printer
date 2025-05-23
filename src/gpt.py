6b70a58b70ad8c54e29f3b41786dc4016520d5c9
Alexxx98 <aleksander.pres98@gmail.com>
2025-05-23 13:32:45 +0200

    text generation changes and print_providers func added

--- src/gpt.py
+++ src/gpt.py
@@ -11,6 +11,11 @@ class GPT:
     client = Client()
 
 
+    def print_providers(self, chat_model: str) -> None:
+        # TODO: List all available providers whith no auhtentication require and for specific chat model.
+        ...
+
+
     def generate_text(self, prompt: str, web_search: bool = False) -> str:
         response = self.client.chat.completions.create(
             model="gpt-4o-mini",
@@ -18,7 +23,13 @@ class GPT:
             web_search=web_search
         )
 
-        return response.choices[0].message.content
+        # content of gpt's response
+        text: str = response.choices[0].message.content
+
+        # get text after the first colon
+        text = (' ').join(text.split(':')[1:])
+        
+        return text
     
 
     def generate_image(self, prompt: str) -> str:
