d0f9d7797439a274f87c76dc6f209381ebc1bc59
Alexxx98 <aleksander.pres98@gmail.com>
2025-05-23 10:58:49 +0200

    output update

--- dev_tools/run.py
+++ dev_tools/run.py
@@ -38,32 +38,32 @@ def main() -> None:
         prompt: str = args[0] if args else None
         if func == "generate_text":
             web_search: bool = args[1] if len(args) > 1 else False
-            return gpt.generate_text(prompt, web_search)
+            print(gpt.generate_text(prompt, web_search))
         
         elif func == "generate_image":
-            return gpt.generate_image(prompt)
+            print(gpt.generate_image(prompt))
 
     # TTS module methods 
     elif module == "tts":
         if func == "print_voices":
             language: str = args[0] if args else "English"
-            return asyncio.run(tts.print_voices(language))
+            asyncio.run(tts.print_voices(language))
         
         elif func == "generate_speech":
             text: str = args[0] if args else None
             voice: str = args[1] if len(args) > 1 else "en-US-ChristopherNeural"
-            return tts.generate_speech(text, voice)
+            tts.generate_speech(text, voice)
       
     # Pexels module methods 
     elif module == "pexels":
         query: str = args[0] if args else None
         if func == "search_for_pohoto":
             curated: bool = args[1] if len(args) > 1 else False
-            return pexels.search_for_photo(query, curated)
+            print(pexels.search_for_photo(query, curated))
 
         elif func == "search_for_video":
             popular: bool = args[1] if len(args) > 1 else False
-            return pexels.search_for_video(query, popular)
+            print(pexels.search_for_video(query, popular))
 
 
 if __name__ == "__main__":
